5f1490020dbed3ba035543ba44c0d2b2fb0f7705
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2024-02-01 19:17:30 -0500

    Laboratorio directivas angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResaltarParesDirective } from './resaltar-pares.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarParesDirective
   ],
   imports: [
     BrowserModule
